3b5d80e048fa8df6087f98c4d1c842c8425389e4
ascanze <ascanze@gmail.com>
2025-09-15 01:39:09 +0200

    Fixed code so philopher and fork goroutines actually communicate over channels properly

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-func philosopher(id int, forks []chan string, wg *sync.WaitGroup) {
+func philosopher(id int, forksReceive []chan string, forksSend []chan string, wg *sync.WaitGroup) {
 	defer wg.Done()
 	left := id
 	right := (id + 1) % 5
@@ -18,27 +18,27 @@ func philosopher(id int, forks []chan string, wg *sync.WaitGroup) {
 		time.Sleep(time.Duration(randRange(200, 800)) * time.Millisecond)
 
 		if id%2 == 0 {
-			<-forks[left]
-			<-forks[right]
+			<-forksReceive[left]
+			<-forksReceive[right]
 		} else {
-			<-forks[right]
-			<-forks[left]
+			<-forksReceive[right]
+			<-forksReceive[left]
 		}
 
 		fmt.Printf("Philosopher: %d is eating his meal %d\n", id, meal)
 		time.Sleep(time.Duration(randRange(200, 800)) * time.Millisecond)
 		meal++
 
-		forks[left] <- "done"
-		forks[right] <- "done"
+		forksSend[left] <- "done"
+		forksSend[right] <- "done"
 	}
 	fmt.Printf("Philosopher: %d is done eating.\n", id)
 }
 
-func fork(forkChan chan string) {
+func fork(forkReceive chan string, forkSend chan string) {
 	for true {
-		forkChan <- "grant"
-		<-forkChan
+		forkReceive <- "grant"
+		<-forkSend
 	}
 }
 
@@ -50,15 +50,17 @@ func main() {
 	var wg sync.WaitGroup
 	wg.Add(5)
 
-	forks := make([]chan string, 5)
+	forksReceive := make([]chan string, 5)
+	forksSend := make([]chan string, 5)
 
 	for i := 0; i < 5; i++ {
-		forks[i] = make(chan string)
-		go fork(forks[i])
+		forksReceive[i] = make(chan string)
+		forksSend[i] = make(chan string)
+		go fork(forksReceive[i], forksSend[i])
 	}
 
 	for i := 0; i < 5; i++ {
-		go philosopher(i, forks, &wg)
+		go philosopher(i, forksReceive, forksSend, &wg)
 	}
 
 	wg.Wait()
